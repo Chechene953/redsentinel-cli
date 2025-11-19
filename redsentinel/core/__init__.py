25d93d6c7910603e9d85cace6fb30b833468e7e8
Chechene953 <147481820+Chechene953@users.noreply.github.com>
2025-11-19 10:02:04 +0100

    commit_V7.2

--- redsentinel/core/__init__.py
+++ redsentinel/core/__init__.py
@@ -3,12 +3,9 @@ RedSentinel Core Module
 Core architecture components
 """
 
-from redsentinel.core.event_bus import EventBus, event_bus
-from redsentinel.core.plugin_manager import PluginManager, plugin_manager
-from redsentinel.core.job_queue import JobQueue, job_queue
-from redsentinel.core.state_machine import StateMachine, ScanState
-from redsentinel.core.config_manager import ConfigManager, config
-from redsentinel.core.api_server import APIServer
+# Lazy imports to avoid circular dependencies
+# Use: from redsentinel.core.config_manager import config
+# Instead of: from redsentinel.core import config
 
 __all__ = [
     'EventBus',
